52baec365ba399a9322b0b5684d0e664d3a7d819
ray1422 <30434835+ray1422@users.noreply.github.com>
2017-07-30 03:30:23 +0800

    Add files via upload

---
+++ script/chat.js
@@ -0,0 +1,103 @@
+var queue = null;
+
+$(document).ready(function(){
+	$("#send").click(function(){
+		onsend();
+	})
+	$("#input").keydown(function(event){
+		if(event.which == 13)
+			onsend();
+	})
+})
+
+function onsend(){
+	if($("#input").val() != ''){
+			if(queue !== null){
+				clearTimeout(queue);
+			}
+			var rd = RandomNumber();
+			$("#chat_wrapper").html($("#chat_wrapper").html() + '<div class="mine"><div class="msg">' + $("#input").val() + '</div></div><a id="'+ rd + '" href="#' + rd + '"></a>');
+			var re = reply($("#input").val());
+			$('#' + rd)[0].click();			
+			$("#input").val('')
+			$("#input")[0].focus('')
+			
+			
+			queue = setTimeout(function(){
+				var rd2 = RandomNumber();
+				$("#chat_wrapper").html($("#chat_wrapper").html() + '<div class="yours"><div class="msg">' + re + '</div></div><a id="'+ rd2 + '" href="#' + rd2 + '"></a>');
+				$('#' + rd2)[0].click();
+				$("#sound_new")[0].play();
+
+			},(2000 * Math.random()) + 3000)
+
+					
+
+	}
+}
+function RandomNumber(){
+    var array1 = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+　　var Str = "";
+    for (var i=1; i<=87; i++)
+    {
+        index = Math.floor(Math.random() * array1.length);
+        Str = Str +array1[index];
+    }
+
+  　return Str;
+
+}
+
+
+function reply(input){
+	
+	var replyText = "恩恩";
+	if(input == ("蝦")
+	|| input.indexOf("蛤") >= 0
+	|| input.indexOf("蝦?") >= 0
+	){
+		var j = 0;
+		var rd = Math.random();
+		var ReQus = new Array();
+		ReQus[j++] = '沒事';
+		ReQus[j++] = '沒什麼';
+		ReQus[j++] = '沒事 今天天氣真好';
+		
+		var i = ~~(rd * ReQus.length);
+		return ReQus[i];
+	}
+	if(input.indexOf("?") >= 0 
+	|| input.indexOf("嗎") >= 0
+	|| input.indexOf("什麼") >= 0
+	){// 問句
+		var j = 0;
+		var rd = Math.random();
+		var ReQus = new Array();
+		ReQus[j++] = '我不知道欸';
+		ReQus[j++] = '我不清楚欸0..0';
+		ReQus[j++] = '問你囉~';
+		
+		var i = ~~(rd * ReQus.length);
+		return ReQus[i];
+	}
+	if(input.indexOf("hi") >= 0 ||
+		input.indexOf("HI") >= 0 ||
+		input.indexOf("Hi") >= 0 ||
+		input.indexOf("嗨") >= 0 ||
+		input.indexOf("安安") >= 0 ||
+		input.indexOf("初次見面") >= 0){// hi
+		var j = 0;
+		var rd = Math.random();
+		var ReQus = new Array();
+		ReQus[j++] = '你好';
+		ReQus[j++] = '安安';
+		ReQus[j++] = '安安你好';
+		ReQus[j++] = 'HI';
+		ReQus[j++] = '初次見面///';
+		
+		var i = ~~(rd * ReQus.length);
+		return ReQus[i];
+	}
+	return replyText;
+}
+
---
+++ script/page.js
@@ -0,0 +1,101 @@
+﻿var userName = '';
+var whoToTalk = '';
+var chatLoaded  = false;
+$(document).ready(function(){
+	page();
+	loading();
+	
+	$('#chat').mouseover(function(){
+		chatStyle();
+	})
+	
+	$("#whoToTalk tr").click(function(){
+		whoToTalk = $(this).data('value');
+		$("#whoToTalk tr").css('background', "#fff");
+		$(this).css('background', '#f4ff5f');
+	})
+	
+});
+
+function goPage(pageTo,pageFrom){	
+	pageFrom = pageFrom!==null ? pageFrom : 0;
+	if(pageFrom !== null){
+		$("[data-page='" + pageFrom + "']").css('opacity', 0);
+		
+		//setTimeout(function(){
+			$("[data-page='" + pageTo + "']").css('display', 'block');
+			$("[data-page='" + pageTo + "']").css('transition', '2s');
+			$("[data-page='" + pageTo + "']").css('opacity', 0);
+		//}
+		setTimeout(function(){
+			$("[data-page='" + pageTo + "']").css('opacity', 1);
+		}, 200);
+		
+		setTimeout(function(){
+			$("[data-page='" + pageFrom + "']").css('display', 'none');
+			
+		}, 2000);
+	}
+	
+}
+
+function nameSend(){
+	if($("#name").val() == ''){
+		alert("主人 請正確的輸入名字呦");
+		return;
+	}
+	else {
+		userName = $("#name").val();
+		goPage(2,1)
+	}
+}
+
+function getInChat() {
+	var avatar = 'unknown.png';
+	if(whoToTalk !== ""){
+		var realName;
+		if(whoToTalk == 'rem'){
+			avatar = 'rem.png';
+			realName = 'レム';
+		}
+		else if(whoToTalk == 'sagiri'){
+			avatar = 'sagiri.jpg';
+			realName = '和泉 紗霧';
+		}
+		else if(whoToTalk == 'shimakaze'){
+			realName = 'しまかぜ';
+		}		
+		goPage(3,2);
+		
+		$("#chat header h4 span").text(realName);
+		setTimeout(function(){goPage(4,3)},5000);
+	}
+}
+
+
+
+
+
+
+function page(){
+	$(".page-wrapper").height($(window).height() * 0.90);
+	$(".page-wrapper").css('top',($(window).height() * 0.05));
+	$(".page-wrapper").css('left',($(window).width() - $(".page-wrapper").width()) / 2);
+	
+
+	
+}
+function loading(){
+	$(".cssload-container").css('top',($(window).height() / 2 - $(".cssload-container").height() / 2))
+	//goPage(0,'null');
+	setTimeout(function(){goPage(1,0)},5000)
+}
+function chatStyle(){
+	if(chatLoaded)return;
+	$("#chat article").css('top',$("#chat header").height())
+	$("#chat article").animate({scrollTop: $("#chat article").height()}, 200);
+	//$("#chat footer").css('top',$("#chat article").height() + $("#chat article").css('top'));
+	$("#chat footer").height($("#chat").height() - $("#chat header").height() - $("#chat article").height())
+	chatLoaded = true;
+}
+
